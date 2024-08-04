10d2563f4f9046844c0de3f5da7b6f19a7c29208
Adrian Perez <adrian1804perez@gmail.com>
2024-08-04 14:45:07 -0700

    Add new and index action with views

--- app/controllers/kittens_controller.rb
+++ app/controllers/kittens_controller.rb
@@ -1,2 +1,26 @@
 class KittensController < ApplicationController
+
+  def index
+    @kittens = Kitten.all
+  end
+
+  def new
+    @kitten = Kitten.new
+  end
+
+  def create
+    @kitten = Kitten.new(kitten_params)
+
+    if @kitten.save
+      redirect_to kittens_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def kitten_params
+    params.require(:kitten).permit(:name, :age, :cuteness, :softness)
+  end
 end
